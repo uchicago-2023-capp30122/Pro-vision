5ff199f8cedf5bc6ade57c830087aec1d743b0c0
Diego Mendoza-Martinez <diegomendozamz@linux2.cs.uchicago.edu>
2023-02-18 04:45:06 -0600

    Working on Dashboard

--- app.py
+++ app.py
@@ -1,64 +1,54 @@
-# Run this app with `python app.py` and
-# visit http://127.0.0.1:8050/ in your web browser.
-
-from dash import Dash, dcc, html
+from dash import Dash, dcc, html, Input, Output
+import urllib.request, json 
+import dash_bootstrap_components as dbc
 import plotly.express as px
 import pandas as pd
+import requests
 
-app = Dash(__name__)
+app = Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
 
 colors = {
     'background': '#111111',
-    'text': '#7FDBFF'
+    'text': '#000000'
 }
 
-df = pd.DataFrame({
-    "Fruit": ["Apples", "Oranges", "Bananas", "Apples", "Oranges", "Bananas"],
-    "Amount": [4, 1, 2, 2, 4, 5],
-    "City": ["SF", "SF", "SF", "Montreal", "Montreal", "Montreal"]
-})
-fig = px.bar(df, x="Fruit", y="Amount", color="City", barmode="group")
-
-df2 = pd.read_csv('https://gist.githubusercontent.com/chriddyp/5d1ea79569ed194d432e56108a04d188/raw/a9f9e8076b837d541398e999dcbac2b2826a81f8/gdp-life-exp-2007.csv')
-fig2 = px.scatter(df2, x="gdp per capita", y="life expectancy",
-                 size="population", color="continent", hover_name="country",
-                 log_x=True, size_max=60)
-
-fig.update_layout(
-    plot_bgcolor=colors['background'],
-    paper_bgcolor=colors['background'],
-    font_color='#7FDBFF'
-)
-
-app.layout = html.Div(style={'backgroundColor': colors['background']}, children=[
-    html.H1(
-        children='Pro-Vision',
-        style={
-            'textAlign': 'center',
-            'color': '#7FDBFF'
-        }
+app.layout = html.Div([
+    html.H1('Pro-Vision'),
+    html.H3('''
+        Visualize the travel time-distance of public facilities/services on 
+        socioeconomic data to identify vulnerabilities in the City of Chicago.
+        '''),
+    html.P("Select a socioeconomic indicator:"),
+    dcc.RadioItems(
+        id='socInd', 
+        options=["Income per Capita", "Neighborhood Safety", "Inequality"],
+        value="Neighborhood Safety",
+        inline=True
     ),
-
-    html.Div(children=
-    '''
-    Visualize the travel time-distance of public facilities/services on 
-    socioeconomic data to identify vulnerabilities in the City of Chicago.
-    ''', style={
-        'textAlign': 'center',
-        'color': '#7FDBFF'
-    }),
-
-    dcc.Graph(
-        id='example-graph',
-        figure=fig
-    ),
-
-    dcc.Graph(
-        id='life-exp-vs-gdp',
-        figure=fig2
-    )
-    
+    dcc.Graph(id="socInd")
 ])
 
+@app.callback(
+    Output("graph", "figure"), 
+    Input("Neighborhood Safety", "value"))
+def display_choropleth(   ):
+    raw_data = requests.get("https://uchicago.box.com/shared/static/mxv5z4eommsiaydm9rru69gj5avygxto.xlsx").content
+    data = pd.read_csv(raw_data, sheet_name = "Community Areas", dtype={'Layer': str, 'Name': str, 'GEOID': int, 
+                                                                      'HCSNS_2016-2018': str})
+    data['HCSNS_2016-2018'] = data['HCSNS_2016-2018'].str.replace(',','')
+    data['HCSNS_2016-2018'] = pd.to_numeric(data['HCSNS_2016-2018'], downcast='int')
+    cleanedData = data.rename(columns={'Name': 'pri_neigh'})
+    # df = px.data.election() 
+    with urllib.request.urlopen("https://data.cityofchicago.org/api/geospatial/bbvz-uum9?method=export&format=GeoJSON") as url:
+        chicagoMap = json.load(url)
+    # geojson = px.data.election_geojson()
+    fig = px.choropleth(
+        cleanedData, geojson=chicagoMap, color='HCSNS_2016-2018',
+        locations="pri_neigh", featureidkey="properties.pri_neigh",
+        range_color=[924, 80076], labels={'unemp':'Number of people whom reported to feel safe'})
+    fig.update_geos(fitbounds="locations", visible=False)
+    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    return fig
+
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(host='127.0.0.1', port='8090', debug=True)
