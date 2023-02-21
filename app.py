c07dab9d13910b244f740f72dfbea9d5bfd2f61e
Diego Mendoza-Martinez <diegomendozamz@linux2.cs.uchicago.edu>
2023-02-20 22:31:19 -0600

    Adjusted dashboard and running basic map

--- app.py
+++ app.py
@@ -22,20 +22,15 @@ cleanedData['HCSNS_2016-2018'] = cleanedData['HCSNS_2016-2018'].str.replace(',',
 # Parsing HCSNS_2016-2018 to numeric
 cleanedData['HCSNS_2016-2018'] = pd.to_numeric(cleanedData['HCSNS_2016-2018'], downcast='float')
 
+# Setting general colors
+colors = { 
+    'bg': 'rgba(0,0,0,0)',
+    'font': '#FFFFFF'
+}
 
 # Importing the city of chicago geojson community level map
 with urllib.request.urlopen("https://uchicago.box.com/shared/static/0hyrwzbja479o01f0ke99b418zvsudjr.geojson") as url:
         chicagoMap = json.load(url)
-
-# Structure of Dashboard
-app.layout = html.Div(children = [
-    html.H1('Pro-Vision'),
-    html.H3('''
-        Visualize the travel time-distance of public facilities/services on 
-        socioeconomic data to identify vulnerabilities in the City of Chicago.
-        '''),
-    html.P("Select a socioeconomic indicator:")
-])
 
 fig = px.choropleth(
         cleanedData, # Socioeconomic data
@@ -44,13 +39,30 @@ fig = px.choropleth(
         color_continuous_scale = 'PuBu', # Choosing color
         locations = 'Community Area', # Identifies locations from geojson
         featureidkey = "properties.pri_neigh", # Consider pri_neigh as key from geojson dictionary
-        # range_color = [924, 80076], # Maybe use this if we want to specify a color range
         labels={'HCSNS_2016-2018': 'Number of people whom reported to feel safe'} # Labelling socioeconomic variable
         )
-fig.update_geos(fitbounds = "locations", visible=False) # Specifies locations
-fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0}) # Sets boundaries
-fig.show() # Shows map
+fig.update_geos(fitbounds = "locations", visible = False) # Maps shapefile boundary locations from geojson
+fig.update_layout(
+    paper_bgcolor = colors['bg'], # Sets transparent background
+    plot_bgcolor = colors['bg'], # Sets transparent background
+    font_color = colors['font'], # Sets font color
+    margin = {"r":20,"t":20,"l":20,"b":20} # Sets boundaries of map
+    ) 
+
+# Structure of Dashboard
+app.layout = html.Div(children = [
+    html.H1('Pro-Vision', style = {'textAlign': 'center'}), # Main header
+    html.H4('''
+        Visualize the travel time-distance of public facilities/services on 
+        socioeconomic data to identify vulnerabilities in the City of Chicago.
+        ''', style = {'textAlign': 'center'}), # Main description
+    html.P("Select a socioeconomic indicator:"), # Paragraph
+    dcc.Graph( 
+        id = 'map',
+        figure = fig
+    ) # Displaying map
+])
 
 # Runs app from terminal with "python3 app.py". Once you run the map, you can exit with 'ctrl + c'
 if __name__ == '__main__':
-    app.run_server(host='127.0.0.1', port='8090', debug=True) # If there is a port error you can change it to '8080'
+    app.run_server(host='127.0.0.1', port='8090', debug = False) # If there is a port error you can change it to '8080'
