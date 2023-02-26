a1a8a8eaef1e33ede137877ca8a6da1e138b0b63
Setu Loomba <setu@linux7.cs.uchicago.edu>
2023-02-26 12:44:50 -0600

    general script for cleaning

--- scripts/data_wrangling.py
+++ scripts/data_wrangling.py
@@ -1,20 +1,41 @@
 import pandas as pd
-from .prov_sdk import *
 
-fire_stations = pd.read_csv(r"raw_data/Fire_stations.csv")
-fire_stations = fire_stations.astype(str)
-fire_stations["full_address"] = fire_stations[
-    ["ADDRESS", "CITY", "STATE", "ZIP"]
-].apply(", ".join, axis=1)
-fire_stations.drop(
-    ["NAME", "ADDRESS", "CITY", "STATE", "ZIP", "ENGINE"], axis=1, inplace=True
+
+def clean_csv(pd_df, col_list, category):
+    """
+    Cleans pandas dataframes
+    Input:
+    pd_df: pandas dataframe
+    col_list: list of relevant columns (should contain Address components)
+    Output: cleaned pandas dataframe
+    """
+    dataframe = pd_df.astype(str)
+    dataframe = dataframe[col_list]
+    dataframe["full_address"] = dataframe[["ADDRESS", "CITY", "STATE", "ZIP"]].apply(
+        ", ".join, axis=1
+    )
+    dataframe["coords"] = dataframe["LOCATION"].str.extract(r"\((.*?)\)")
+    dataframe["coords"] = dataframe["coords"].apply(
+        lambda x: tuple(map(float, x.split(",")))
+    )
+    dataframe.drop("LOCATION", axis=1, inplace=True)
+    dataframe["type"] = category
+    dataframe["type"] = dataframe["type"].astype("category")
+    return dataframe
+
+
+fire_stations = pd.read_csv(r"raw_data/Fire_Stations.csv")
+warming_centers = pd.read_csv(r"raw_data/Warming_Centers.csv")
+neighbor_clinics = pd.read_csv(r"raw_data/Neighborhood_Health_Clinics_-_Historical.csv")
+police_stations = pd.read_csv(r"raw_data/Police_Stations.csv")
+list_cols = ["ADDRESS", "CITY", "STATE", "ZIP", "LOCATION"]
+fire_clean = clean_csv(fire_stations, list_cols, "fire_station")
+warm_clean = clean_csv(warming_centers, list_cols, "warming_center")
+clinics_clean = clean_csv(
+    neighbor_clinics,
+    list_cols,
+    "neighborhood_clinic",
 )
-fire_stations["coords"] = fire_stations["LOCATION"].str.extract(r"\((.*?)\)")
-fire_stations["coords"] = fire_stations["coords"].apply(
-    lambda x: tuple(map(float, x.split(",")))
-)
-fire_stations.drop("LOCATION", axis=1, inplace=True)
-fire_stations["isochrone"] = fire_stations.apply(
-    lambda row: isochrone(row["coords"], row["full_address"]), axis=1
-)
-fire_stations.to_csv("data/fire_stations.csv")
+police_clean = clean_csv(police_stations, list_cols, "police_station")
+provisions = pd.concat([fire_clean, warm_clean, clinics_clean, police_clean], axis=0)
+provisions.to_csv("data/provisions.csv")
--- scripts/geo_code.py
+++ scripts/geo_code.py
@@ -1,32 +1,31 @@
 import googlemaps
 import csv
-#hide key
-api_key = 'AIzaSyABteA2oGa6yJK1u92PAvbKhcpMiiE21w8'
-gmaps = googlemaps.Client(key=api_key)
-#Read from CSV or JSON
-#address_directory = csv.DictReader("filename")
-address_directory = {
-    'Warming Centers' : ['1140 West 79th Street, Chicago, IL 60620',
-                         '10 South Kedzie Avenue, Chicago, IL 60612',
-                         '4314 South Cottage Grove, Chicago, IL 60653',
-                         '845 West Wilson Avenue, Chicago, IL 60640',
-                         '8650 South Commercial Avenue, Chicago, IL 60617',
-                         '4312 West North Avenue, Chicago, IL 60639'
-                         ]
-}
-# def geocode(directory):
-#     coord_directory = {}
-#     for prov,locations in directory.items():
-#         coord_directory[prov]=[]
-#         for location in locations:
-#             geocode_result = gmaps.geocode(location)
-#             lat = geocode_result[0]['geometry']['location']['lat']
-#             long = geocode_result[0]['geometry']['location']['lng']
-#             coord_directory[prov].append((location,lat,long))
-#     return coord_directory
+from datetime import datetime
+from traveltimepy import Driving, Coordinates, TravelTimeSdk
 
+# hide key
+api_key = "AIzaSyABteA2oGa6yJK1u92PAvbKhcpMiiE21w8"
+gmaps = googlemaps.Client(key=api_key)
+sdk = TravelTimeSdk(
+    app_id="f9571a60", api_key="4caa1f5a4b29f582b455df38a1004541", limit_per_host=4
+)
+
+#add delay to geocode
 def geocode(address):
-    geocode_result = gmaps.geocode(location)
-    lat = geocode_result[0]['geometry']['location']['lat']
-    long = geocode_result[0]['geometry']['location']['lng']
-    return (lat,long)
+    geocode_result = gmaps.geocode(address)
+    lat = geocode_result[0]["geometry"]["location"]["lat"]
+    long = geocode_result[0]["geometry"]["location"]["lng"]
+    return (lat, long)
+
+
+def isochrone(coords, address):
+    if coords is None:
+        coords = geocode(address)
+    alat, along = coords
+    result = sdk.time_map(
+        coordinates=[Coordinates(lat=alat, lng=along)],
+        departure_time=datetime.now(),
+        transportation=Driving(),
+        travel_time=600,
+    )
+    return result
