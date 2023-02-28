82728f02a3c0d8fc3f005305b344d1d5400e3cf5
Diego Mendoza-Martinez <diegomendozamz@linux2.cs.uchicago.edu>
2023-02-28 14:58:30 -0600

    Added interactivity to map with dropdown menus

--- app.py
+++ app.py
@@ -1,28 +1,32 @@
 # These are the packages. You may need to pip install some of them.
 # These should be included in the virtual environment
-from dash import Dash, dcc, html
+from dash import Dash, dcc, html, Input, Output
 import urllib.request, json 
-import dash_bootstrap_components as dbc
 import plotly.express as px 
 import pandas as pd
-import numpy as np
+import dash_bootstrap_components as dbc
+# import numpy as np
 import requests
-import openpyxl
-from plotly.subplots import make_subplots
+# import openpyxl
+import copy as cp
+# from plotly.subplots import make_subplots
 
-app = Dash(__name__, external_stylesheets=[dbc.themes.DARKLY]) # dbc.themes.DARKLY is an imported theme for the Dash
+app = Dash(__name__, external_stylesheets=[dbc.themes.FLATLY])
+
+app.config.suppress_callback_exceptions = True
 
 #------- DATA FOR SOCIOECONOMIC VARIABLE -------#
 # Importing from direct link
-raw_data = requests.get("https://uchicago.box.com/shared/static/mxv5z4eommsiaydm9rru69gj5avygxto.xlsx").content
+raw_data = requests.get("https://uchicago.box.com/shared/static/ooyygpkhml7vd6p170e3tgl303cfkkek.xlsx").content
 # Reading file to pandas
 cleanedData = pd.read_excel(raw_data, sheet_name = "Community areas", dtype={'Layer': str, 'Name': str, 'GEOID': int, 
-                                                                      'HCSNS_2016-2018': str})
+                                                                      'SocEconVar': str, 'Value': str, 'Quintiles': int,
+                                                                      'Label': str})
 cleanedData = cleanedData.rename(columns={'Name': 'Community Area'}) 
-# Getting rid of commas in the variable HCSNS_2016-2018
-cleanedData['HCSNS_2016-2018'] = cleanedData['HCSNS_2016-2018'].str.replace(',','')
-# Parsing HCSNS_2016-2018 to numeric
-cleanedData['HCSNS_2016-2018'] = pd.to_numeric(cleanedData['HCSNS_2016-2018'], downcast='float')
+# Getting rid of commas in the variable Value
+cleanedData['Value'] = cleanedData['Value'].str.replace(',','')
+# Parsing Value to numeric
+cleanedData['Value'] = pd.to_numeric(cleanedData['Value'], downcast='float')
 
 #------- GEOJSON TO DISPLAY SOCIOECONOMIC VARIABLE -------#
 # Importing the city of chicago geojson community level map
@@ -31,57 +35,114 @@ with urllib.request.urlopen("https://uchicago.box.com/shared/static/0hyrwzbja479
 
 #------- DATA TO DISPLAY PUBLIC SERVICES/PROVISIONS -------#
 # Importing from direct link
-raw_data2 = requests.get("https://uchicago.box.com/shared/static/uhmit09c6imq79gxwmfpkooxcph8j4t7.xlsx").content
+raw_data2 = requests.get("https://uchicago.box.com/shared/static/tuq2cw0d18e6wsa4mixvos1g1lp5z3yp.xlsx").content
 # Reading file to pandas
 cleanedData2 = pd.read_excel(raw_data2, sheet_name = "Police_Stations_-_Map", 
-                            dtype={'DISTRICT': str, 'ADDRESS': str, 'ZIP': str, 
+                            dtype={'Provision': str, 'ADDRESS': str, 'ZIP': str, 
                                     'latitude': float, 'longitud': float})
 
 # Setting general colors
 colors = { 
     'bg': 'rgba(0,0,0,0)',
-    'font': '#FFFFFF'
+    'font': ' #767676'
 }
 
-#------- CHOROPLETH MAP TO DISPLAY SOCIOECONOMIC VARIABLE -------#
-trace1 = px.choropleth(
-        cleanedData, # Socioeconomic data
-        geojson = chicagoMap, # Community Level Geojson
-        color = 'HCSNS_2016-2018', # Setting color intensity by the values of HCSNS_2016-2018 (i.e., safety variable)
-        color_continuous_scale = 'PuBu', # Choosing color
-        locations = 'Community Area', # Identifies locations from geojson
-        featureidkey = "properties.pri_neigh", # Consider pri_neigh as key from geojson dictionary
-        labels = {'HCSNS_2016-2018': 'Number of people whom reported to feel safe'}, # Labelling socioeconomic variable
-        )
-trace1.update_geos(fitbounds = "locations", visible = False) # Maps shapefile boundary locations from geojson
-
-#------- SCATTER MAP FOR PUBLIC SERVICES/PROVISIONS -------#
-fig = px.scatter_geo(cleanedData2, lat="latitude", lon="longitud", 
-                        hover_data=["DISTRICT", "ADDRESS", "ZIP"],
-                        color_discrete_sequence=["fuchsia"])
-
-fig.add_trace(trace1.data[0])
-trace1.layout.update(showlegend=True)
-fig.layout.update(
-    paper_bgcolor = colors['bg'], # Sets transparent background
-    plot_bgcolor = colors['bg'], # Sets transparent background
-    font_color = colors['font'], # Sets font color
-    margin = {"r":20,"t":20,"l":20,"b":20} # Sets boundaries of map
-    )
+#------- NAVIGATION BAR -------#
+UChi_logo = "https://www.lib.uchicago.edu/static/base/images/unvlogo-white.png"
+navbar = dbc.Navbar(
+    dbc.Container(
+        [
+            html.A(
+                dbc.Row(
+                    [
+                        dbc.Col(html.Img(src=UChi_logo, height="30px")),
+                        dbc.Col(dbc.NavbarBrand("Pro-Vision")),
+                    ],
+                    align="center",
+                ),
+                href="#",
+                style={"textDecoration": "none", 'font-family': 'Times New Roman'},
+            ),
+        ]
+    ),
+    color = "#800000",
+    dark = True,
+)
 
 #------- DASHBOARD STRUCTURE -------#
 app.layout = html.Div(children = [
-    html.H1('Pro-Vision', style = {'textAlign': 'center'}), # Main header
-    html.H4('''
+    navbar, # Navigation bar
+    html.Div([]), # Space
+    html.H5('''
         Visualize the travel time-distance of public facilities/services on 
         socioeconomic data to identify vulnerabilities in the City of Chicago.
-        ''', style = {'textAlign': 'center'}), # Main description
-    html.P("Select a socioeconomic indicator:"), # Paragraph
-    dcc.Graph( 
-        id = 'map',
-        figure = fig
-    ) # Displaying choropleth map
+        ''', style = {'font-family': 'Gotham', 'margin' : '50px'}), # Main description
+    html.Div([
+            dcc.Dropdown(
+                cleanedData['SocEconVar'].unique(),
+                placeholder = 'Select a Socioeconomic Variable',
+                searchable = False,
+                id = 'SocEconVar'
+            )], style = {'width': '48%', 'display': 'inline-block'}),
+    html.Div([
+            dcc.Dropdown(
+                cleanedData2['Provision'].unique(),
+                placeholder = 'Select a Public Facility',
+                searchable = False,
+                id = 'ProvisionVar'
+            )], style = {'width': '48%', 'display': 'inline-block'}),
+    html.Div(id = 'map') # Displaying joined map
 ])
+
+
+@app.callback(
+    Output(component_id = 'map', component_property = 'children'),
+    Input(component_id = 'SocEconVar', component_property = 'value'),
+    Input(component_id = 'ProvisionVar', component_property = 'value')
+)
+def update_figure(SocEconValue, ProvisionValue):
+
+    global cleanedData
+    global cleanedData2
+    global chicagoMap
+
+    socEconData = cleanedData[cleanedData['SocEconVar'] == SocEconValue]
+    provisionsData = cleanedData2[cleanedData2['Provision'] == ProvisionValue]
+
+    #------- CHOROPLETH MAP TO DISPLAY SOCIOECONOMIC VARIABLE -------#
+    trace1 = px.choropleth(
+            socEconData, # Socioeconomic data
+            geojson = chicagoMap, # Community Level Geojson
+            color = 'Value', # Setting color intensity by the values of HCSNS_2016-2018 (i.e., safety variable)
+            color_continuous_scale = 'PuBu', # Choosing color
+            locations = 'Community Area', # Identifies locations from geojson
+            featureidkey = "properties.pri_neigh", # Consider pri_neigh as key from geojson dictionary
+            labels = 'Label', # Labelling socioeconomic variable
+            )
+    trace1.update_geos(fitbounds = "locations", visible = False) # Maps shapefile boundary locations from geojson
+    trace1.layout.update(
+        margin = {"r":20,"t":20,"l":20,"b":20}, # Sets boundaries of map
+        showlegend = True # Show legend
+        )
+
+    base_map = cp.deepcopy(trace1) # Creating a base map to add layers
+
+    #------- SCATTER MAP FOR PUBLIC SERVICES/PROVISIONS -------#
+    trace2 = px.scatter_geo(provisionsData, lat = 'latitude', lon = 'longitud', 
+                            hover_data={'Provision': False, 'longitud': False, 
+                                        'latitude': False, 'ADDRESS': True, 
+                                        'ZIP': True})
+    trace2.layout.update(
+        paper_bgcolor = colors['bg'], # Sets transparent background
+        plot_bgcolor = colors['bg'], # Sets transparent background
+        font_color = colors['font'], # Sets font color
+        margin = {"r":20,"t":20,"l":20,"b":20} # Sets boundaries of map
+        )
+
+    #------- FULL MAP WITH JOINED TRACES -------#
+    joined_map = base_map.add_trace(trace2.data[0])
+
+    return [dcc.Graph(figure = joined_map)]
 
 # Runs app from terminal with "python3 app.py". Once you run the map, you can exit with 'ctrl + c'
 if __name__ == '__main__':
