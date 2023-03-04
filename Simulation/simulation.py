fed5220552b5911e251f477557a5f60102d1ddf5
Angel Rodriguez Gonzalez <angelrodriguezg@linux1.cs.uchicago.edu>
2023-03-04 02:07:53 -0600

    Nx to adjust

--- Simulation/simulation.py
+++ Simulation/simulation.py
@@ -6,6 +6,7 @@ import networkx as nx
 import urllib.request, json
 from point_in_area import point_in_area
 import matplotlib.pyplot as plt
+import random
 
 
 
@@ -53,6 +54,9 @@ class Network(object):
         df_extended.drop('geoid10', axis = 1, inplace = True) 
         df_extended['Prov_within'] = df['geometry'].apply(\
             lambda x: 1 if point_in_area(self.prov_centers['coords_geo'], x) else 0)
+        
+        # Include 1 col for each pol station, to have the distance between this and the com_area
+        df_extended['Min_dist'] = df_extended[[]].apply(min, axis = 1)  # Complete with the missing cols
         self.df_extended = df_extended
   
         pass
@@ -70,35 +74,83 @@ class Network(object):
         """
 
         G = nx.Graph()
-        # Iterate over the districts and add them as nodes to the graph
-        for i, _ in self.com_areas.iterrows():
-            G.add_node(i)
+        # Iterate over the districts and add them as nodes to the graph, with the label
+        for _, com in self.df_extended.iterrows():
+            if com['Tensioned'] == 1 and com['Prov_within'] == 1:
+                G.add_node(com['Name'], tensioned = 1, prov = 1)
+            elif com['Tensioned'] == 1 and j['Prov_within'] == 0:
+                G.add_node(com['Name'], tensioned = 1, prov = 0)
+            elif com['Tensioned'] == 0 and j['Prov_within'] == 1:
+                G.add_node(com['Name'], tensioned = 0, prov = 1)
+            else:
+                G.add_node(com['Name'], tensioned = 0, prov = 0)
 
-        # Iterate over the districts again and check which ones are neighboring
-        for i, com in self.com_areas.iterrows():
-            for j, other_com in self.com_areas.iterrows():
-                if i == j:
-                    continue
-                if com.geometry.touches(other_com.geometry):
-                    G.add_edge(i, j)        
-        # Include labels
+        # Iterate over the districts again and check which ones are neighboring.
+        #   EXTREMELY EXPENSIVE!! num_iter = (77!/2)-77
+        for i, com in self.df_extended.iterrows():
+            for j, other_com in self.df_extended.iterrows():
+                if i < j and com['geometry'].touches(other_com['geometry']):
+                    G.add_edge(com['Name'], other_com['Name'])        
+
+        self.model = G
+        
+        # Include the savefile as in demo_nx 
 
         pass
 
 
-    def apply_shock(self, ):
+
+        # Some issues to be solved here:
+            # to create the nodes, we can avoid the ugly for loop
+            # we have to avoid the nested for loop. Options:
+            #   investigate whether another nx method
+            #   Setu's new API
+            #   clustering using lat 
+            #   clustering using the GeoID (worst option)
+
+
+
+
+    def apply_shock_com_areas(self, ):
         """
-        Creates a diagonal matrix for modelling either a negative shock in the 
-            amount of provision or a change in tensionned census tracts. Each 
-            element of the diagonal incorporates the value of the shock for each
-            census tract. Applies the shock by multiplying the adjacency matrix
-            to this shock matrix.
-            the shock by  
+        Generates a random shock in the degree of tension for all the community 
+            areas: the assignation of top-tensioned areas is stochastically 
+            modified.
         Parameters:
         Returns        
         """
 
-        df_shock = self.df_extended # risk of modifying self.df_extended?
-        # df_shock['Tensioned_shock'] = 
-        # df_shock['Prov_within_shock'] = 
+        self.df_shock_com = self.df_extended
+        self.df_shock_com['Tensioned_sim'] = 0
+        shock_rows = np.random.choice(\
+            self.df_shock_com.index, size = 10, replace = False)
+        self.df_shock_com.loc[shock_rows, 'Tensioned_sim'] = 1
+        self.df_shock_com['Min_dist_shock'] = self.df_shock_com[[]].apply(min, axis = 1)  # Complete with the missing cols
 
+        # Modify the graph label
+
+        return self.df_shock_com
+
+
+
+    def apply_shock_prov_centers(self, reduction = 0.2):
+        """
+        Generates a random shock in the set of provision centers: the number of
+            operating centers is reduced by a factor and the eliminated centers
+            are stochastically selected.
+        Parameters:
+        Returns        
+        """
+
+        self.df_shock_prov = self.df_extended
+        prov_centers_all = list(self.self.df_extended.columns.values)
+        prov_centers_shock = random.sample(prov_centers_all, \
+                                           round(len(self.prov_centers)*(1-reduction)))
+
+        self.df_shock_prov['Min_dist_shock'] = self.df_shock_prov[\
+            prov_centers_shock].apply(min, axis = 1) 
+        # For the node label: self.df_shock_prov['Prov_within_shock'] = 
+
+        # Modify the graph label
+
+        return self.df_shock_prov
