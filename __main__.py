ef544b49f4f8e8e7ee6ddd7e876d2be7b70202ad
Setu Loomba <setu@linux7.cs.uchicago.edu>
2023-03-08 19:38:04 -0600

    Updated 17:38

--- __main__.py
+++ __main__.py
@@ -2,11 +2,11 @@ import sys
 import os
 if sys.argv[1] == "quick":
     from .dashboard_quick import app as quick_app
-    sys.path.append(os.path.abspath(os.path.join('Pro-vision', 'dashboard_quick')))
+    sys.path.append(os.path.abspath(os.path.join('ProVision', 'dashboard_quick')))
     quick_app.app.run_server(host='127.0.0.1', port='4444', debug = False)
 elif sys.argv[1] == "full":
     from .dashboard_full import app as full_app
-    sys.path.append(os.path.abspath(os.path.join('Pro-vision', 'dashboard_full')))
+    sys.path.append(os.path.abspath(os.path.join('ProVision', 'dashboard_full')))
     full_app.app.run_server(host='127.0.0.1', port='4444', debug = False)
 else:
     print("Please enter one argument only")
